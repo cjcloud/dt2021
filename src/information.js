c999b10d75ba3d800d4bf9e01d72ba323f2bf64d
CJ Cloud <cjcloudsolutions@gmail.com>
2020-03-01 05:03:42 +0000

    Update 6

--- src/information.js
+++ src/information.js
@@ -26,11 +26,19 @@ export default function SimpleCard() {
                     <h5>Create a shortcut to your home screen</h5>
 
                     <ul>
-                        <p className="bullet-title">On Android and iPhone</p>
+                        <p className="bullet-title">On iPhone</p>
                         <li>Whilst on Team tables - Go to "Create Bookmark"</li>
                         <li>Scroll down to find "add to Homescreen"</li>
                         <li>This will create an icon on your phone that you can use to access the app without having to enter the web address </li>
                     </ul>
+
+                    <ul>
+                        <p className="bullet-title">On Android</p>
+                        <li>Whilst on Team tables - Access dropdown menu form three dots located at the top right of the screen</li>
+                        <li>Scroll down to find "add to Homescreen"</li>
+                        <li>This will create an icon on your phone that you can use to access the app without having to enter the web address </li>
+                    </ul>
+                    <img src="../public/footballWhite.svg" />
 
 
                 </Typography>
